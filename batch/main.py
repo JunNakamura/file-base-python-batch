533a1051890b4729a81b499a5b09a34ec936646f
中村順 <n.slenderness@gmail.com>
2021-07-22 10:59:52 +0900

    feat: add entry point, logging and setup.py

---
+++ batch/main.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import yaml
+from logging import config, getLogger
+from app_name import APP_NAME
+
+logger = getLogger(APP_NAME).getChild(__name__)
+
+
+def main():
+    logger.info('start sample batch')
+
+
+if __name__ == '__main__':
+    Path('logs').mkdir(exist_ok=True)
+    logging_config_file = Path('logging.yaml')
+    with logging_config_file.open() as f:
+        log_dict_config = yaml.safe_load(f)
+        config.dictConfig(log_dict_config)
+    main()
---
+++ batch/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="file-base-batch",
+    version="1.0.0",
+    install_requires=[
+        "PyYaml",
+    ],
+    url="https://github.com/JunNakamura/file-base-python-batch",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9',
+)
