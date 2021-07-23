d69e6c504f810859f3ada224fb357c8959ff66e8
中村順 <n.slenderness@gmail.com>
2021-07-23 16:13:06 +0900

    refactor: add abstract class for batch with file lock

---
+++ batch/file_base_batch.py
@@ -0,0 +1,51 @@
+from backup.backup_directory import BackupDirectory
+from decorator.logging import timed
+from input.entry_file import EntryFile
+from input.input_directory import InputDirectory
+from input.order_history_file import OrderHistoryFile
+from mixin.batch import BatchWithFileLock
+from app_name import APP_NAME
+from logging import getLogger
+
+from output.output_directory import OutputDirectory
+from output.result_file import ResultFile
+from work.work_directory import WorkDirectory
+from work.work_entry_file import WorkEntryFile
+from work.work_order_history_file import WorkOrderHistoryFile
+
+logger = getLogger(APP_NAME).getChild(__name__)
+
+
+class FileBaseBatch(BatchWithFileLock):
+
+    def __init__(self):
+        super().__init__('file_base_batch')
+
+    @timed
+    def execute(self):
+        input_directory = InputDirectory()
+        if not input_directory.trigger_file.exists():
+            logger.info(f'trigger file({input_directory.trigger_file}) does not exist. nothing to do.')
+            return
+        input_directory.trigger_file.unlink()
+        logger.info('start process since trigger file exists')
+        order_history_file = OrderHistoryFile(input_directory)
+        entry_file = EntryFile(input_directory)
+        input_files = [order_history_file, entry_file]
+        for input_file in input_files:
+            logger.info(f'checksum of {input_file.name}: {input_file.md5_checksum()}')
+
+        work_directory = WorkDirectory()
+        work_order_history_file = WorkOrderHistoryFile(order_history_file, work_directory)
+        work_entry_file = WorkEntryFile(entry_file, work_directory)
+
+        output_directory = OutputDirectory()
+        output_directory.trigger_file.unlink(missing_ok=True)
+
+        result_file = ResultFile(output_directory, work_directory)
+        result_file.create(work_order_history_file, work_entry_file)
+        output_directory.trigger_file.touch()
+
+        backup_directory = BackupDirectory()
+        work_directory.move_files_to(backup_directory)
+        work_directory.remove_if_exists()
---
+++ batch/mixin/batch.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from decorator.logging import timed
+from file_lock import PIDFileLock
+from app_name import APP_NAME
+from logging import getLogger
+
+logger = getLogger(APP_NAME).getChild(__name__)
+
+
+class BatchWithFileLock(ABC):
+
+    def __init__(self, batch_name: str):
+        self.__name = batch_name
+
+    @property
+    def name(self):
+        return self.__name
+
+    def start(self):
+        lock = PIDFileLock(self.name)
+        if lock.is_locked:
+            logger.info(f'stop this process of {self.name} since another process({lock.pid()}) is in progress.')
+            return
+        with lock:
+            self.execute()
+
+    @abstractmethod
+    def execute(self):
+        raise NotImplementedError
